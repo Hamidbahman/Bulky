735d1495bd3178f65a78921adcfde550673f4ec1
Hamid <h.bahman.90@gmail.com>
2024-11-04 04:36:57 +0000

    CompanyCRUD

--- Bulky.DataAccess/Repository/IRepository/IUnitOfWork.cs
+++ Bulky.DataAccess/Repository/IRepository/IUnitOfWork.cs
@@ -5,6 +5,7 @@ namespace Bulky.DataAccess.Repository.IRepository
         ICategoryRepository Category{get;}
         IProductRepository Product{get;}
         ICompanyRepository Company { get; }
+        IShoppingCartRepository ShoppingCart { get; }
 
         void Save();
     }
--- Bulky.DataAccess/Repository/UnitOfWork.cs
+++ Bulky.DataAccess/Repository/UnitOfWork.cs
@@ -1,6 +1,8 @@
 using System.Data.Common;
+using System.Security.Cryptography;
 using Bulky.DataAccess.Data;
 using Bulky.DataAccess.Repository.IRepository;
+using Bulky.Models;
 
 namespace Bulky.DataAccess.Repository
 {
@@ -10,12 +12,15 @@ namespace Bulky.DataAccess.Repository
         public ICategoryRepository Category{get; private set;}
         public IProductRepository Product{get; private set;}
         public ICompanyRepository Company{get; private set;}
+        public IShoppingCartRepository ShoppingCart{get; private set;}
+
         public UnitOfWork(ApplicationDbContext db)
         {
             _db = db;
             Category = new CategoryRepository(_db);
             Product = new ProductRepository(_db);
             Company = new CompanyRepository(_db);
+            ShoppingCart = new ShoppingCartRepository(_db);
         }
 
         public void Save()
